35ec347c3828f1a5d3d86f16987a60e63946998c
yhfreeman <yhfreeman12@gmail.com>
2021-07-13 22:12:13 -0700

    remove redundant due to what ships w/ win server 2016 by default

--- fme/vagrant/scripts/ReverseAccessControl.ps1
+++
@@ -1,8 +0,0 @@
-# Source https://docs.ansible.com/ansible/latest/user_guide/windows_setup.html#host-requirements
-
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-
-$reg_winlogon_path = "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon"
-Set-ItemProperty -Path $reg_winlogon_path -Name AutoAdminLogon -Value 0
-Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultUserName -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $reg_winlogon_path -Name DefaultPassword -ErrorAction SilentlyContinue
